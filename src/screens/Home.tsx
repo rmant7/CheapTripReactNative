91b7f6a31d290d7b8aa6b619572e94b75af4db66
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2023-01-01 23:24:14 +0200

    doing autocomplete

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, FC } from "react";
 import { View, StyleSheet, Image } from "react-native";
 import { Text, useTheme, TextInput, Button } from "react-native-paper";
 
+import Autocomplete from "react-native-autocomplete-input";
+
 import { FontAwesome5 } from "@expo/vector-icons";
 
-export const Home = () => {
+export interface Item {
+  name: string;
+}
+
+export const Home: FC = () => {
   const [forCity, setForCity] = useState("");
   const [toCity, setToCity] = useState("");
+  const [cities, setCities] = useState([]);
+  const [hideForCity, setHideForCity] = useState(false);
+  const [hideToCity, setHideToCity] = useState(false);
   const theme = useTheme();
+
+  const filtredCities = (forCity: string) => {
+    if (forCity) {
+      const cityFor = cities.filter((item: { name: string }) => {
+        const name = item.name;
+        if (name === undefined) return;
+        return name.toLowerCase().indexOf(forCity.toLowerCase()) > -1;
+      });
+      return cityFor;
+    }
+    return [];
+  };
+
+  const dataForCity = filtredCities(forCity);
+  const dataToCity = filtredCities(toCity);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const getCities = await fetch(
+          "https://graphproject-482d9-default-rtdb.europe-west1.firebasedatabase.app/locations.json"
+        );
+        const responseData = await getCities.json();
+
+        await setCities(responseData);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+  // const renderItems = ({ item }: { item: Item }) => <Text>{item.name}</Text>;
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
@@ -23,26 +63,84 @@ export const Home = () => {
         </Text>
       </View>
       <View style={styles.mainContainer}>
-        <TextInput
+        {/* <TextInput
           style={styles.input}
           label="From"
           value={forCity}
           onChangeText={(text) => setForCity(text)}
           mode={"outlined"}
+        /> */}
+
+        <Autocomplete
+          containerStyle={{
+            width: "100%",
+            backgroundColor: "tomato",
+          }}
+          listContainerStyle={{}}
+          listStyle={{}}
+          hideResults={hideForCity}
+          data={dataForCity}
+          value={forCity}
+          onChangeText={(text: string) => {
+            setForCity(text), setHideForCity(false);
+          }}
+          flatListProps={{
+            keyExtractor: (_: any, idx: any) => idx,
+            renderItem: ({ item }: { item: Item }) => (
+              <Text
+                onPress={() => {
+                  setForCity(item.name);
+                  setHideForCity(true);
+                }}
+                style={styles.autocompleteText}
+              >
+                {item.name}
+              </Text>
+            ),
+          }}
         />
+
         <FontAwesome5
           style={styles.iconArrow}
           name="angle-double-down"
           size={24}
           color={`${theme.colors.primary}`}
         />
-        <TextInput
+        <Autocomplete
+          containerStyle={{
+            width: "100%",
+            backgroundColor: "tomato",
+          }}
+          listContainerStyle={{}}
+          listStyle={{}}
+          hideResults={hideToCity}
+          data={dataToCity}
+          value={toCity}
+          onChangeText={(text: string) => {
+            setToCity(text), setHideToCity(false);
+          }}
+          flatListProps={{
+            keyExtractor: (_: any, idx: any) => idx,
+            renderItem: ({ item }: { item: Item }) => (
+              <Text
+                onPress={() => {
+                  setToCity(item.name);
+                  setHideToCity(true);
+                }}
+                style={styles.autocompleteText}
+              >
+                {item.name}
+              </Text>
+            ),
+          }}
+        />
+        {/* <TextInput
           style={styles.input}
           label="To"
           value={toCity}
           onChangeText={(text) => setToCity(text)}
           mode={"outlined"}
-        />
+        /> */}
       </View>
       <View style={styles.buttonContainer}>
         <Button
@@ -106,5 +204,16 @@ const styles = StyleSheet.create({
     height: "100%",
 
     resizeMode: "contain",
+  },
+  autocompleteContainer: {
+    // flex: 1,
+    // left: 0,
+    // position: "absolute",
+    // right: 0,
+    // top: 0,
+    // zIndex: 1,
+  },
+  autocompleteText: {
+    fontSize: 26,
   },
 });
